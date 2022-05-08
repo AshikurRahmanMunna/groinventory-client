eaff1342fcd1842d2066d2ede165ce2cafeb0df8
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-05-08 13:25:12 +0600

    Messenger added

---
+++ src/Pages/Shared/Messenger/Messenger.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat/lib/MessengerCustomerChat";
+
+const Messenger = () => {
+  return (
+    <div>
+      <MessengerCustomerChat
+        pageId="104722065573692"
+        appId="1168493543948897"
+      />
+    </div>
+  );
+};
+
+export default Messenger;
